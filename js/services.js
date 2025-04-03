26dd02c6ca65ba0d6d2d16c8e44854bdb37c2d68
SofiaNords <sofia.k.nordstrom@gmail.com>
2025-04-03 13:41:21 +0200

    Modify content in service modals

--- js/services.js
+++ js/services.js
@@ -3,7 +3,7 @@ const services = [
     id: 1,
     name: "Psykologsamtal Vuxen",
     breifDescription: "Första mötet handlar om att kartlägga dina behov och tillsammans hitta en väg framåt. Jag hjälper dig att formulera vad du vill ha hjälp med och anpassar behandlingen efter dina förutsättningar. Som legitimerad psykolog erbjuder jag samtal kring bland annat stress, ångest, relationsproblem, trauma och livskriser. Jag arbetar med KBT och erbjuder individuell behandling.",
-    description: "Första mötet får du berätta vad du vill ha hjälp med. Jag lyssnar in och ställer frågor för att göra en bedömning av vad du kan vara hjälpt av. Mot slutet av mötet ger jag ett förslag på vad som kan vara hjälpsamt för dig. Jag anpassar behandlingen efter dina behov. Känner du dig osäker på vad du önskar hjälp med eller har svårt att formulera det i ord - Kom som du är, jag hjälper dig i mötet att ringa in problemet. Som legitimerad psykolog har jag tystnadsplikt. Samtalet kan handla om stress, utmattning, nedstämdhet, oro, ångest, sömnproblem, låg självkänsla, negativ självbild, livs- eller identitetskris, existentiella kriser, relationssvårigheter, sorg, trauma eller neuropsykologiska/kognitiva svårigheter. Kanske har du en svår situation eller något personligt dilemma som du behöver sortera i. Kanske har du varit med om något svårt som du behöver bearbeta. Kanske känner du att livet skaver men har svårt att finna orsaken. Jag arbetar utifrån kognitiv beteendeterapi (KBT) och har lång erfarenhet av psykologisk bedömning och behandling. Mottagningen är öppen för besök endast efter tidsbokning. Kontakta mig om du inte hittar en tid som passar dig, så försöker vi lösa det.",
+    description: "Första mötet får du berätta vad du vill ha hjälp med. Jag lyssnar in och ställer frågor för att göra en bedömning av vad du kan vara hjälpt av. Mot slutet av mötet ger jag ett förslag på vad som kan vara hjälpsamt för dig. Jag anpassar behandlingen efter dina behov. Känner du dig osäker på vad du önskar hjälp med eller har svårt att formulera det i ord - Kom som du är, jag hjälper dig i mötet att ringa in problemet.<br><br>Som legitimerad psykolog har jag tystnadsplikt. Samtalet kan handla om stress, utmattning, nedstämdhet, oro, ångest, sömnproblem, låg självkänsla, negativ självbild, livs- eller identitetskris, existentiella kriser, relationssvårigheter, sorg, trauma eller neuropsykologiska/kognitiva svårigheter. Kanske har du en svår situation eller något personligt dilemma som du behöver sortera i. Kanske har du varit med om något svårt som du behöver bearbeta. Kanske känner du att livet skaver men har svårt att finna orsaken.<br><br>Jag arbetar utifrån kognitiv beteendeterapi (KBT) och har lång erfarenhet av psykologisk bedömning och behandling. Mottagningen är öppen för besök endast efter tidsbokning. Kontakta mig om du inte hittar en tid som passar dig, så försöker vi lösa det.",
     price45: 1200,
     price75: 1900
     },
@@ -11,16 +11,33 @@ const services = [
     id: 2,
     name: "Psykologsamtal Ungdom 13-18 år",
     breifDescription: "Första mötet rekommenderas att förälder eller vårdnadshavare följer med. Tillsammans kartlägger vi dina behov och skapar en plan för behandlingen, som anpassas efter dig. Vi kan prata om stress, ångest, låg självkänsla, oro eller svåra livssituationer. Jag arbetar med KBT och erbjuder stöd i att hantera dina utmaningar.",
-    description: "Jag rekommenderar att förälder/vårdnadshavare följer med till första mötet. Vanligtvis börjar vi första samtalet med att prata en stund tillsammans med förälder/vårdnadshavare för att ta reda på vad du behöver hjälp med. Ibland pratar jag en stund ensam med dig i mitten av mötet medan förälder/vårdnadshavare väntar i väntrummet. Mot slutet av mötet återkopplar jag vad som kan vara hjälpsamt för dig. Jag anpassar behandlingen efter dina behov. Om vi bokar fler möten kan det bli att vi pratar ensamma utan förälder/vårdnadshavare. Som legitimerad psykolog har jag tystnadsplikt. Vi kan prata om stress, oro, ångest, sömnproblem, nedstämdhet, negativ självbild eller dåligt självförtroende. Kanske är du en i jobbig situation som du behöver stöd i. Kanske har du varit med om något svårt som du behöver bearbeta. Känner du dig osäker på vad du önskar hjälp med eller har svårt att formulera det i ord - Kom som du är, jag hjälper dig i mötet att ringa in problemet. Jag arbetar utifrån kognitiv beteendeterapi (KBT) och har lång erfarenhet av psykologisk bedömning och behandling. Mottagningen är öppen för besök endast efter tidsbokning. Kontakta mig om du inte hittar en tid som passar dig, så försöker vi lösa det.",
+    description: "Jag rekommenderar att förälder/vårdnadshavare följer med till första mötet. Vanligtvis börjar vi första samtalet med att prata en stund tillsammans med förälder/vårdnadshavare för att ta reda på vad du behöver hjälp med. Ibland pratar jag en stund ensam med dig i mitten av mötet medan förälder/vårdnadshavare väntar i väntrummet. Mot slutet av mötet återkopplar jag vad som kan vara hjälpsamt för dig. Jag anpassar behandlingen efter dina behov. Om vi bokar fler möten kan det bli att vi pratar ensamma utan förälder/vårdnadshavare.<br><br>Som legitimerad psykolog har jag tystnadsplikt. Vi kan prata om stress, oro, ångest, sömnproblem, nedstämdhet, negativ självbild eller dåligt självförtroende. Kanske är du en i jobbig situation som du behöver stöd i. Kanske har du varit med om något svårt som du behöver bearbeta. Känner du dig osäker på vad du önskar hjälp med eller har svårt att formulera det i ord - Kom som du är, jag hjälper dig i mötet att ringa in problemet.<br><br>Jag arbetar utifrån kognitiv beteendeterapi (KBT) och har lång erfarenhet av psykologisk bedömning och behandling. Mottagningen är öppen för besök endast efter tidsbokning. Kontakta mig om du inte hittar en tid som passar dig, så försöker vi lösa det.",
     price45: 1200,
     price75: 1900
     },
     {
-    id: 3,
-    name: "Arbetslivsinriktad psykologisk coaching",
-    breifDescription: "Arbetslivsinriktad psykologisk coaching hjälper dig att utvecklas i ditt yrkesliv. Jag använder forskningsbaserade tekniker från KBT för att hjälpa dig hitta din väg framåt, sätta och nå mål, överkomma hinder, hantera stress och förbättra din prestation.",
-    description: "Arbetslivsinriktad psykologisk coaching passar dig som önskar stöd att utvecklas i ditt yrkesliv. Jag jobbar med forskningsbaserade tekniker från kognitiv beteendeterapi. Som legitimerad psykolog har jag tystnadsplikt. Arbetslivsinriktad psykologisk coaching kan hjälpa dig: <br/>-Hitta din väg framåt i arbetslivet<br/>-Frigöra din potential<br/>-Formulera och nå uppsatta mål<br/>-Identifiera och överkomma hinder<br/>-Hantera svårigheter i arbetssituationen och avvägningar mellan arbete och privatliv<br/>-Öka ditt välbefinnande och minska stress<br/>-Förbättra din prestation<br/>-Öka din förmåga att lösa problem<br/>-Få rätt saker gjorda och minska uppskjutandebeteende<br/>Mottagningen är öppen för besök endast efter tidsbokning. Kontakta mig om du inte hittar en tid som passar dig, så försöker vi lösa det.",
-    price45: 1200,
-    price75: 1900
+        id: 3,
+        name: "Arbetslivsinriktad psykologisk coaching",
+        breifDescription: "Arbetslivsinriktad psykologisk coaching hjälper dig att utvecklas i ditt yrkesliv. Jag använder forskningsbaserade tekniker från KBT för att hjälpa dig hitta din väg framåt, sätta och nå mål, överkomma hinder, hantera stress och förbättra din prestation.",
+        description: `
+            Arbetslivsinriktad psykologisk coaching passar dig som önskar stöd att utvecklas i ditt yrkesliv. Jag jobbar med forskningsbaserade tekniker från kognitiv beteendeterapi. Som legitimerad psykolog har jag tystnadsplikt.
+            <br>
+            <br>
+            Arbetslivsinriktad psykologisk coaching kan hjälpa dig att:
+            <ul>
+                <li>Hitta din väg framåt i arbetslivet</li>
+                <li>Frigöra din potential</li>
+                <li>Formulera och nå uppsatta mål</li>
+                <li>Identifiera och överkomma hinder</li>
+                <li>Hantera svårigheter i arbetssituationen och avvägningar mellan arbete och privatliv</li>
+                <li>Öka ditt välbefinnande och minska stress</li>
+                <li>Förbättra din prestation</li>
+                <li>Öka din förmåga att lösa problem</li>
+                <li>Få rätt saker gjorda och minska uppskjutandebeteende</li>
+            </ul>
+            Mottagningen är öppen för besök endast efter tidsbokning. Kontakta mig om du inte hittar en tid som passar dig, så försöker vi lösa det.
+        `,
+        price45: 1200,
+        price75: 1900
     }
 ]
