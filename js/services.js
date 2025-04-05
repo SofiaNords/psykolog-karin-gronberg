c6cc67e16fddf6b80d77abbe8ccaef2c9ff4391d
SofiaNords <sofia.k.nordstrom@gmail.com>
2025-04-05 10:24:41 +0200

    Update service modals
    - Add images to service modals
    - Replace description & briefDescription
    - Add price
    - Adjust card-body structure
    - Style card

--- js/services.js
+++ js/services.js
@@ -2,42 +2,40 @@ const services = [
     {
     id: 1,
     name: "Psykologsamtal Vuxen",
-    breifDescription: "Första mötet handlar om att kartlägga dina behov och tillsammans hitta en väg framåt. Jag hjälper dig att formulera vad du vill ha hjälp med och anpassar behandlingen efter dina förutsättningar. Som legitimerad psykolog erbjuder jag samtal kring bland annat stress, ångest, relationsproblem, trauma och livskriser. Jag arbetar med KBT och erbjuder individuell behandling.",
-    description: "Första mötet får du berätta vad du vill ha hjälp med. Jag lyssnar in och ställer frågor för att göra en bedömning av vad du kan vara hjälpt av. Mot slutet av mötet ger jag ett förslag på vad som kan vara hjälpsamt för dig. Jag anpassar behandlingen efter dina behov. Känner du dig osäker på vad du önskar hjälp med eller har svårt att formulera det i ord - Kom som du är, jag hjälper dig i mötet att ringa in problemet.<br><br>Som legitimerad psykolog har jag tystnadsplikt. Samtalet kan handla om stress, utmattning, nedstämdhet, oro, ångest, sömnproblem, låg självkänsla, negativ självbild, livs- eller identitetskris, existentiella kriser, relationssvårigheter, sorg, trauma eller neuropsykologiska/kognitiva svårigheter. Kanske har du en svår situation eller något personligt dilemma som du behöver sortera i. Kanske har du varit med om något svårt som du behöver bearbeta. Kanske känner du att livet skaver men har svårt att finna orsaken.<br><br>Jag arbetar utifrån kognitiv beteendeterapi (KBT) och har lång erfarenhet av psykologisk bedömning och behandling. Mottagningen är öppen för besök endast efter tidsbokning. Kontakta mig om du inte hittar en tid som passar dig, så försöker vi lösa det.",
-    price45: 1200,
-    price75: 1900
+    image: "../assets/images/vuxen.webp",
+    breifDescription: "Individuella samtal för vuxna som söker stöd i livets olika faser. Vi arbetar med metoder som passar din personliga utveckling.",
+    description: "Första mötet får du berätta vad du vill ha hjälp med. Jag lyssnar in och ställer frågor för att göra en bedömning av vad du kan vara hjälpt av. Mot slutet av mötet ger jag ett förslag på vad som kan vara hjälpsamt för dig. Jag anpassar behandlingen efter dina behov. Känner du dig osäker på vad du önskar hjälp med eller har svårt att formulera det i ord - Kom som du är, jag hjälper dig i mötet att ringa in problemet.<br><br>Som legitimerad psykolog har jag tystnadsplikt. Samtalet kan handla om stress, utmattning, nedstämdhet, oro, ångest, sömnproblem, låg självkänsla, negativ självbild, livs- eller identitetskris, existentiella kriser, relationssvårigheter, sorg, trauma eller neuropsykologiska/kognitiva svårigheter. Kanske har du en svår situation eller något personligt dilemma som du behöver sortera i. Kanske har du varit med om något svårt som du behöver bearbeta. Kanske känner du att livet skaver men har svårt att finna orsaken.<br><br>Jag arbetar utifrån kognitiv beteendeterapi (KBT) och har lång erfarenhet av psykologisk bedömning och behandling.</br></br>50 minuters samtal 1200 kr.</br></br>Mottagningen är öppen för besök endast efter tidsbokning. Kontakta mig om du inte hittar en tid som passar dig, så försöker vi lösa det.",
     },
     {
     id: 2,
     name: "Psykologsamtal Ungdom 13-18 år",
-    breifDescription: "Första mötet rekommenderas att förälder eller vårdnadshavare följer med. Tillsammans kartlägger vi dina behov och skapar en plan för behandlingen, som anpassas efter dig. Vi kan prata om stress, ångest, låg självkänsla, oro eller svåra livssituationer. Jag arbetar med KBT och erbjuder stöd i att hantera dina utmaningar.",
-    description: "Jag rekommenderar att förälder/vårdnadshavare följer med till första mötet. Vanligtvis börjar vi första samtalet med att prata en stund tillsammans med förälder/vårdnadshavare för att ta reda på vad du behöver hjälp med. Ibland pratar jag en stund ensam med dig i mitten av mötet medan förälder/vårdnadshavare väntar i väntrummet. Mot slutet av mötet återkopplar jag vad som kan vara hjälpsamt för dig. Jag anpassar behandlingen efter dina behov. Om vi bokar fler möten kan det bli att vi pratar ensamma utan förälder/vårdnadshavare.<br><br>Som legitimerad psykolog har jag tystnadsplikt. Vi kan prata om stress, oro, ångest, sömnproblem, nedstämdhet, negativ självbild eller dåligt självförtroende. Kanske är du en i jobbig situation som du behöver stöd i. Kanske har du varit med om något svårt som du behöver bearbeta. Känner du dig osäker på vad du önskar hjälp med eller har svårt att formulera det i ord - Kom som du är, jag hjälper dig i mötet att ringa in problemet.<br><br>Jag arbetar utifrån kognitiv beteendeterapi (KBT) och har lång erfarenhet av psykologisk bedömning och behandling. Mottagningen är öppen för besök endast efter tidsbokning. Kontakta mig om du inte hittar en tid som passar dig, så försöker vi lösa det.",
-    price45: 1200,
-    price75: 1900
+    image: "../assets/images/ungdom.webp",
+    breifDescription: "Samtal för ungdomar mellan 13-18 i trygg miljö. Vi anpassar efter dina behov.",
+    description: "Jag rekommenderar att förälder/vårdnadshavare följer med till första mötet. Vanligtvis börjar vi första samtalet med att prata en stund tillsammans med förälder/vårdnadshavare för att ta reda på vad du behöver hjälp med. Ibland pratar jag en stund ensam med dig i mitten av mötet medan förälder/vårdnadshavare väntar i väntrummet. Mot slutet av mötet återkopplar jag vad som kan vara hjälpsamt för dig. Jag anpassar behandlingen efter dina behov. Om vi bokar fler möten kan det bli att vi pratar ensamma utan förälder/vårdnadshavare.<br><br>Som legitimerad psykolog har jag tystnadsplikt. Vi kan prata om stress, oro, ångest, sömnproblem, nedstämdhet, negativ självbild eller dåligt självförtroende. Kanske är du en i jobbig situation som du behöver stöd i. Kanske har du varit med om något svårt som du behöver bearbeta. Känner du dig osäker på vad du önskar hjälp med eller har svårt att formulera det i ord - Kom som du är, jag hjälper dig i mötet att ringa in problemet.<br><br>Jag arbetar utifrån kognitiv beteendeterapi (KBT) och har lång erfarenhet av psykologisk bedömning och behandling.</br></br>50 minuters samtal 1200 kr.</br></br>Mottagningen är öppen för besök endast efter tidsbokning. Kontakta mig om du inte hittar en tid som passar dig, så försöker vi lösa det.",
     },
     {
         id: 3,
         name: "Psykologisk coaching",
-        breifDescription: "Samtal som hjälper dig att utvecklas i både privatliv och arbetsliv. Hjälper dig att hitta din väg framåt och gå mot det liv du önskar leva.",
+        image: "../assets/images/coaching.webp",
+        breifDescription: "Psykologisk coaching passar dig som önskar professionell vägledning i att utvecklas i ditt privatliv eller yrkesliv. Hjälper dig identifiera och nå dina drömmar och mål.",
         description: `
-            Arbetslivsinriktad psykologisk coaching passar dig som önskar stöd att utvecklas i ditt yrkesliv. Jag jobbar med forskningsbaserade tekniker från kognitiv beteendeterapi. Som legitimerad psykolog har jag tystnadsplikt.
+            Coaching skiljer sig från psykologisk behandling på så vis att coaching fokuserar på personlig utveckling och måluppfyllelse, medan behandling fokuserar på att förbättra din psykiska hälsa. Coaching riktar sig till dig som vill förbättra prestation eller livskvalitet även om du inte lider av psykisk ohälsa. Coaching är oftast korta och målinriktade insatser, medan behandling kan vara mer långvarig och djupgående. Jag jobbar med forskningsbaserade tekniker från kognitiv beteendeterapi. Som legitimerad psykolog har jag tystnadsplikt.
             <br>
             <br>
-            Arbetslivsinriktad psykologisk coaching kan hjälpa dig att:
+            Psykologisk coaching kan hjälpa dig att:
             <ul>
-                <li>Hitta din väg framåt i arbetslivet</li>
+                <li>Hitta din väg framåt</li>
                 <li>Frigöra din potential</li>
                 <li>Formulera och nå uppsatta mål</li>
                 <li>Identifiera och överkomma hinder</li>
-                <li>Hantera svårigheter i arbetssituationen och avvägningar mellan arbete och privatliv</li>
+                <li>Hantera svårigheter och avvägningar mellan arbete och privatliv</li>
                 <li>Öka ditt välbefinnande och minska stress</li>
                 <li>Förbättra din prestation</li>
                 <li>Öka din förmåga att lösa problem</li>
                 <li>Få rätt saker gjorda och minska uppskjutandebeteende</li>
             </ul>
+            50 minuters samtal 1200 kr.</br></br>
             Mottagningen är öppen för besök endast efter tidsbokning. Kontakta mig om du inte hittar en tid som passar dig, så försöker vi lösa det.
-        `,
-        price45: 1200,
-        price75: 1900
+        `
     }
 ]
