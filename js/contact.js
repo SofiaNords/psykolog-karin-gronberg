94c8886500449b5ec666f0485fffe5c0afc5783d
SofiaNords <sofia.k.nordstrom@gmail.com>
2025-07-02 20:46:31 +0200

    Add Kaddio contact iframe
    - Add placholder image
    - Create contact.js
    - Update services.js from 12 years
    - Update booking.js heigth 100%
    - Update index.html from 12 years
    - Add contact iframe-wrapper in index.html
    - Add karin-nlt-mobile image
    - Add contact-placeholder image
    - Style contact iframe solution in style.css

--- js/contact.js
+++ js/contact.js
@@ -1,21 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const bookingWrapper = document.getElementById('booking-wrapper');
-    const placeholder = document.getElementById('booking-placeholder');
-    const overlay = document.getElementById('booking-overlay');
+    const contactWrapper = document.getElementById('contact-wrapper');
+    const placeholder = document.getElementById('contact-placeholder');
+    const overlay = document.getElementById('contact-overlay');
 
-    if (!bookingWrapper || !overlay) return;
+    if (!contactWrapper || !overlay) return;
 
     const frame = document.createElement('iframe');
-    frame.src = "https://karingronbergab.kaddio.com/iframe/booking";
+    frame.src = "https://karingronbergab.kaddio.com/iframe/contact-us";
     frame.width = "100%";
     frame.height = "100%";
     frame.style.border = "0";
     frame.setAttribute("scrolling", "yes");
     frame.setAttribute("loading", "lazy");
 
-    const activateBooking = () => {
-        bookingWrapper.appendChild(frame);
-
+    const activateContact = () => {
+        contactWrapper.appendChild(frame);
         placeholder.style.opacity = 0;
         overlay.style.opacity = 0;
 
@@ -25,5 +24,5 @@ document.addEventListener("DOMContentLoaded", () => {
         }, 1000);
     };
 
-    overlay.addEventListener('click', activateBooking);
+    overlay.addEventListener('click', activateContact);
 });
